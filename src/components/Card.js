854cc82075aa5d0816ae2f2c34fcbe047ad1d930
PawanKumar <pawan5501kumar2001@gmail.com>
2026-02-11 14:14:44 +0530

    Fix ESLint CI build errors

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Cardc(props) {
-    return (
-        <>
-            <div className={`card cw m-3 bg-${props.mode}`}>
-                <img className="card-img-top" src={props.src} alt="Card image cap"/>
-                    <div className={`card-body`}>
-                        <Link className={`card-text nav-link text-${props.mode==="light"?"dark":"light"}`} to={props.destin}>{props.name}</Link>
-                        <Link className={`card-text nav-link text-${props.mode==="light"?"dark":"light"}`} to={props.destin}>{props.price}</Link>
-                    </div>
-            </div>
-        </>
-    )
+  return (
+    <div className={`card cw m-3 bg-${props.mode}`}>
+      <img
+        className="card-img-top"
+        src={props.src}
+        alt={props.name}   
+      />
+
+      <div className="card-body">
+        <Link
+          className={`card-text nav-link text-${
+            props.mode === "light" ? "dark" : "light"
+          }`}
+          to={props.destin}
+        >
+          {props.name}
+        </Link>
+
+        <Link
+          className={`card-text nav-link text-${
+            props.mode === "light" ? "dark" : "light"
+          }`}
+          to={props.destin}
+        >
+          {props.price}
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 Cardc.defaultProps = {
-    name: "Product name",
-    price: "Product price"
-  }
+  name: "Product Name",
+  price: "Product Price",
+};
--- src/components/Cardsec.js
+++ src/components/Cardsec.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Cardsec(props) {
-    return (
-        <>
-            <div className={`card bg-${props.mode} text-white m-4 csw`}>
-                <img className="card-img " src={props.src} />
-                <div className="card-img-overlay d-flex justify-content-center align-items-center">
-                    <Link to={props.destin} className='card-title h1 text-decoration-none'>
-                        <h5 className="card-title h1">{props.title}</h5>
-                    </Link>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className={`card bg-${props.mode} text-white m-4 csw`}>
+      <img
+        className="card-img"
+        src={props.src}
+        alt={props.title}   // ✅ Added alt text
+      />
+
+      <div className="card-img-overlay d-flex justify-content-center align-items-center">
+        <Link
+          to={props.destin}
+          className="card-title h1 text-decoration-none text-white"
+        >
+          {props.title}
+        </Link>
+      </div>
+    </div>
+  );
 }
